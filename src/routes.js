6b045518deeb67dc872129a588aa8a6f260efd6e
Jamiu Mojolagbe <jamiumojolagbe@wpa-7-687.cc.umanitoba.ca>
2018-05-17 18:42:12 -0500

    basic crud operations completed

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import Knex from './knex';
+import GUID from 'node-uuid';  // uuid module can be used instead
+
 const Joi = require('joi');
+//const Joi = Hapi.types;
 
 const routes = [
     {  
@@ -7,11 +10,13 @@ const routes = [
         path: '/',
         handler: (request, response) => response('Welcome to Pet Shelter API')
     },
+    
     {  
         method: 'GET',
         path: '/welcome/{name}',
         handler: (request, response) => response(`Welcome ${request.params.name} to Pet Shelter API`)
     },
+    
     {
         path: '/pets',
         method: 'GET',
@@ -32,13 +37,45 @@ const routes = [
 
                 })
                 .catch(error => {
-                    reply('Server-Side Error >> '+error);
+                    response('Server-Side Error >> '+error);
                 });
 
         }
     },
+    
     {
-        
+        path: '/pets/{id}',
+        method: 'GET',
+        handler: (request, response) => {
+            const read = Knex('pets').where({id: request.params.id})
+                .select('name','type','breed','location', 'longitude', 'latitude','picture_url' )
+                .then(pet => {
+                    if(!pet || pet.length === 0) {
+                        response({
+                            error: true,
+                            errMessage: `Pet with ID = ${request.params.id} not found!`
+                        });
+                    }
+                    response({
+                        pet
+                        //pet: results
+                    });
+
+                })
+                .catch(error => {
+                    response('Server-Side Error >> '+error);
+                });
+        },
+        config: {
+            validate: {
+                params: {
+                    id: Joi.number().integer().min(1)
+                }
+            }
+        }
+    },
+    
+    {
         path: '/pets',
         method: 'POST',
         handler: (request, response) => {
@@ -57,7 +94,7 @@ const routes = [
                     message: `Pet (${pet.name}) successfully added.`
                 });
             }).catch(error => {
-                reply(`Server-Side Error >> ${error}`);
+                response(`Server-Side Error >> ${error}`);
             });
         },
         config: {
@@ -70,6 +107,76 @@ const routes = [
                     latitude: Joi.number().required(),
                     longitude: Joi.number().required(),
                     picture_url: Joi.string().uri().trim().required()
+                }
+            }
+        }
+    },
+    
+    {
+        method: 'PUT',
+        path:   '/pets/{id}',
+        handler: (request, response) => {
+            const pet = request.payload;  // formdata or json
+            const updates = Knex('pets')
+            .where({id: request.params.id}) 
+            .update({
+                name: pet.name,
+                type: pet.type,
+                breed: pet.breed,
+                location: pet.location,
+                latitude: pet.latitude,
+                longitude: pet.longitude,
+                picture_url: pet.picture_url
+            })
+            .then(result => {
+                response({
+                    message: `Pet (${pet.name}) successfully updated.`
+                });
+            })
+            .catch(error => {
+                response(`Server-Side Error >> ${error}`);
+            });
+                
+        },
+        config: {
+            validate: {
+                payload: {
+                    name: Joi.string().alphanum().min(3).max(100).required(),
+                    type: Joi.string().min(3).max(100).required(),
+                    breed: Joi.string().min(3).max(100).required(),
+                    location: Joi.string().min(3).max(100).required(),
+                    latitude: Joi.number().required(),
+                    longitude: Joi.number().required(),
+                    picture_url: Joi.string().uri().trim().required()
+                },
+                params: {
+                    id: Joi.number().integer().min(1)
+                }
+            }
+        }
+    },
+    
+    {
+        path: '/pets/{id}',
+        method: 'DELETE',
+        handler: (request, response) => {
+            const del = Knex('pets')
+                .where({id: request.params.id})
+                .del()
+                .then(result => {
+                    response({
+                        message: `Pet with ID = ${request.params.id} successfully deleted`
+                    });
+
+                })
+                .catch(error => {
+                    response('Server-Side Error >> '+error);
+                });
+        },
+        config: {
+            validate: {
+                params: {
+                    id: Joi.number().integer().min(1)
                 }
             }
         }
